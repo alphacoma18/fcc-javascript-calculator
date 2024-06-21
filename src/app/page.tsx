7c1c12fb34e080954cd28517f559468ec2c380f7
Alpha Romer Coma <alpharomercoma@proton.me>
2024-06-21 23:42:17 +0800

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import { JavaScriptCalculator } from "@/components/javascript-calculator";
+export default function Home() {
+  return (
+    <main>
+      <JavaScriptCalculator />
+    </main>
+  );
+}
